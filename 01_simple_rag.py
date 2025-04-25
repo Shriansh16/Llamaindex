52cc54162245c8ced07fe5c7404a82088e7a1b9c
Shriansh16 <shrianshsingh20@gmail.com>
2025-04-25 23:58:47 +0530

    updated

--- 01_simple_rag.py
+++ 01_simple_rag.py
@@ -1,8 +1,8 @@
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 from dotenv import load_dotenv
-
+from llama_index.llms import OpenAI
 load_dotenv()
-
+OpenAI(model="gpt-3.5-turbo")
 documents = SimpleDirectoryReader("pdf/").load_data()
 
 index = VectorStoreIndex.from_documents(documents) 
